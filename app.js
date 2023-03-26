05d82e52c5c7dfb655c860bb9a5fddf2a0f17b1d
mohammadmirancarsales <mohammad.miran@carsales.com.au>
2023-03-26 11:23:34 +0530

    ep2

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM  from "react-dom";
 /**
  *
  * <div id="parent">
@@ -16,7 +18,7 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h1", {}, "I'm an h1 m tag"),
       React.createElement("h2", {}, "I'm an h2 tag"),
     ]),
     React.createElement("div", { id: "child2" }, [
